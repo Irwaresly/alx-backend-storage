4fbf00e5e3c0737e42682ce802982bd2fb91d001
Irwaresly <silviairware372@gmail.com>
2024-05-09 06:04:28 -0700

    changes

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++
@@ -1,7 +0,0 @@
--- A SQL script that creates a table (called users) with following fields
--- id, name, email
-CREATE TABLE IF NOT EXISTS users (
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
-)
